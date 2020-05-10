24e842a24e40a0e1f231211f970046ca5b8978e9
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-10 23:28:08 +0545

    CRUDL

--- frontend/django-react/src/App.js
+++ frontend/django-react/src/App.js
@@ -9,26 +9,30 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import Button from './components/utils/button.component'
 import Forms from './components/utils/forms.component'
-
+import { Route, Switch } from 'react-router-dom'
+import Detail from './components/main/Detail.component'
+import { Link } from 'react-router-dom'
 class App extends React.Component {
   state = {
     list: [],
-    hideButton: false,
     like: 0,
     dislike: 0,
     title: '',
     content: '',
   }
 
-  async componentDidMount() {
-    await axios.get('http://127.0.0.1:8000/api/').then((res) => {
-      this.setState({ list: res.data, hideButton: true })
-      return res
+  componentDidMount() {
+    this.axiosGetRequest('http://127.0.0.1:8000/api/').then((res) => {
+      this.setState({ list: res.data })
     })
   }
+
+  async axiosGetRequest(url) {
+    return await axios.get(url)
+  }
   handleLikeClick(id) {}
+
   async handleFormSubmit(evt) {
-    console.log(evt)
     evt.preventDefault()
     await axios
       .post('http://127.0.0.1:8000/api/', {
@@ -37,7 +41,7 @@ class App extends React.Component {
       })
       .then(async () => {
         this.setState({ title: '', content: '' })
-        return await axios.get('http://127.0.0.1:8000/api/')
+        return this.axiosGetRequest('http://127.0.0.1:8000/api/')
       })
       .then((res) => this.setState({ list: res.data }))
       .catch((error) => console.log(error))
@@ -49,7 +53,9 @@ class App extends React.Component {
     const listArray = this.state.list.map((item) => (
       <div class="card text-left my-2">
         <div class="card-body">
-          <h4 class="card-title">{item.title}</h4>
+          <Link to={`/detail/${item.id}`}>
+            <h4 class="card-title">{item.title}</h4>
+          </Link>
           <p class="card-text">{item.content}</p>
           <div className=" card-footer row justify-content-between bg-white">
             <div className="row">
@@ -66,6 +72,18 @@ class App extends React.Component {
     ))
     return (
       <div className="container">
+        <Switch>
+          <Route
+            exact
+            path="/detail/:id"
+            render={(routeProps) => (
+              <Detail
+                axiosGetRequest={this.axiosGetRequest.bind(this)}
+                routeProps={routeProps}
+              />
+            )}
+          />
+        </Switch>
         <Forms
           handleFormSubmit={this.handleFormSubmit.bind(this)}
           handleChange={this.handleChange.bind(this)}
@@ -75,9 +93,6 @@ class App extends React.Component {
         <div className="App d-flex flex-column justify-content-center">
           {listArray.reverse()}
         </div>
-        {!this.state.hideButton ? (
-          <div className="d-flex justify-content-center"></div>
-        ) : null}
       </div>
     )
   }
--- frontend/django-react/src/index.js
+++ frontend/django-react/src/index.js
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1 className="text-center">Django React App</h1>
-    <App />
+    <BrowserRouter>
+      <h1 className="text-center">Django React App</h1>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
