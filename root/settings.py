dad307b598e4f8c5b6e3be2c32b2df54b06f1287
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-12 20:40:39 +0545

    time to deploy

--- root/settings.py
+++ root/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise..django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cors Config
 
